78f60937ec531f8b2972fe4878f652060aa57a75
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2023-11-15 21:25:22 +0530

    Add files via upload

---
+++ 01 Variables/variables.js
@@ -0,0 +1,13 @@
+// type of data in variables can be changed at runtime in js
+
+var a=1;
+console.log(a);
+a="hello";
+console.log(a);
+
+console.log("\n")
+
+let b=1;
+console.log(b);
+b="hello";
+console.log(b);
