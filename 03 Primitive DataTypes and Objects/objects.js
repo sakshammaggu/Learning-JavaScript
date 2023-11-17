a5eedf251013b44bf083703af214bc46275f9f64
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2023-11-17 13:46:01 +0530

    Add files via upload

---
+++ 03 Primitive DataTypes and Objects/objects.js
@@ -0,0 +1,11 @@
+const object={
+    name:"abc",
+    age: 18
+}
+console.log(object);
+
+object.name="def";
+console.log(object);
+
+object["age"]=19;
+console.log(object);
---
+++ 03 Primitive DataTypes and Objects/primitive_datatypes.js
@@ -0,0 +1,23 @@
+/*
+    7 primtive data types - NN SS BB U
+
+    Null
+    Number
+
+    String
+    Symbol
+
+    Boolean
+    BigInt
+
+    Undefined
+*/
+let a=null;
+let b=123;
+let c="saksham";
+let d=Symbol("This is symbol");
+let e=true;
+let f=BigInt(456);
+let g=undefined;
+let h;
+console.log(a, "\n", b, "\n", d, "\n", e, "\n", f, "\n", g, "\n", h);
