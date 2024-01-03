4afc2537e8086514b9351d67090e2e97c6eb8721
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2024-01-03 20:47:55 +0530

    Add files via upload

---
+++ 07 Strings/strings.js
@@ -0,0 +1,16 @@
+let str1="abc";
+console.log(str1);
+
+let str2='def';
+console.log(str2);
+
+console.log(`${str1} and ${str2} are strings`); // template literals (used backtick)
+
+// string methods
+let str="saksham";
+console.log(str);
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(2, 4));
+console.log(str.replace("s", "a"));
