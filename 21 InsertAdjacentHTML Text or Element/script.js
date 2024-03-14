26bf6c0d0de908c22a9fe9797eddd71efdb91931
Saksham Maggu <114247233+sakshammaggu@users.noreply.github.com>
2024-03-14 22:06:45 +0530

    Add files via upload

---
+++ 21 InsertAdjacentHTML Text or Element/script.js
@@ -0,0 +1,11 @@
+let id1=document.getElementById("first");
+id1.insertAdjacentHTML("beforebegin", "<div>beforebegin</div>");
+id1.insertAdjacentHTML("afterbegin", "<div>afterbegin</div>");
+id1.insertAdjacentHTML("beforeend", "<div>beforeend</div>");
+id1.insertAdjacentHTML("afterend", "<div>afterend</div>");
+
+let id2=document.getElementById("second");
+id2.insertAdjacentHTML("beforebegin", "<div>beforebegin</div>");
+id2.insertAdjacentHTML("afterbegin", "<div>afterbegin</div>");
+id2.insertAdjacentHTML("beforeend", "<div>beforeend</div>");
+id2.insertAdjacentHTML("afterend", "<div>afterend</div>");
